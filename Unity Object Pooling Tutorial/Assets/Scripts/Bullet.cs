49cd3cd389a4e3e3d32b9feea278087961a14b04
ibmaciel <ibmaciel@users.noreply.github.com>
2017-08-17 11:44:08 -0300

    Add bullet and ship

---
+++ Unity Object Pooling Tutorial/Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 5;
+
+	void Update () {
+		transform.Translate (0, speed * Time.deltaTime, 0);
+	}
+}
---
+++ Unity Object Pooling Tutorial/Assets/Scripts/BulletFire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour {
+
+	public float fireTime = 0.5f;
+	public GameObject bullet;
+
+	void Start () {
+		InvokeRepeating ("Fire", fireTime, fireTime);
+	}
+
+	void Fire () {
+		Instantiate (bullet, transform.position, Quaternion.identity);
+	}
+}
